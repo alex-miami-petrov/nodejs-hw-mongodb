95ff9ac7e5297d3cb79a94887b7cc4187bca7b1a
Miami <alex.miami.petrov@gmail.com>
2024-11-20 16:11:09 +0200

    m

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,11 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (error, req, res, next) => {
+  if (isHttpError(error) === true) {
+    return res
+      .status(error.statusCode)
+      .send({ status: error.statusCode, message: error.message });
+  }
   console.error(error);
-  res.status(500).send({ status: 500, message: 'Internal server error' });
+  res.status(500).send({ status: 500, message: 'Something went wrong' });
 };
