d939f500fae6fa92bf7eefb8f43f2799a7fe6ece
Miami <alex.miami.petrov@gmail.com>
2024-11-24 16:56:27 +0200

    m

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,30 +1,18 @@
 import Contact from '../models/contact.js';
 
 const getAllContacts = async () => {
-  try {
-    const contacts = await Contact.find();
-    return contacts;
-  } catch (error) {
-    throw new Error('Error retrieving contacts');
-  }
+  const contacts = await Contact.find();
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const contact = await Contact.findById(contactId);
-    return contact;
-  } catch (error) {
-    throw new Error('Error retrieving contact');
-  }
+  const contact = await Contact.findById(contactId);
+  return contact;
 };
 
 const addContact = async (contactData) => {
-  try {
-    const newContact = await Contact.create(contactData);
-    return newContact;
-  } catch (error) {
-    throw new Error('Error creating contact');
-  }
+  const newContact = await Contact.create(contactData);
+  return newContact;
 };
 
 const updateContact = async (contactId, payload, options = {}) => {
