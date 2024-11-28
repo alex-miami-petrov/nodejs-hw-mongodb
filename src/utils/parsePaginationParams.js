c01a323b6a668dcd5aa2fccc5ca177fed332247a
Miami <alex.miami.petrov@gmail.com>
2024-11-28 16:31:53 +0200

    sort_final

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -15,6 +15,9 @@ export const parsePaginationParams = (query) => {
   const parsedPage = parseNumber(page, 1);
   const parsedPerPage = parseNumber(perPage, 10);
 
+  // const parsedPage = Math.max(parseInt(page, 10) || 1, 1); - це ж покращення?
+  // const parsedPerPage = Math.min(Math.max(parseInt(perPage, 10) || 10, 1), 100); - це ж покращення?
+
   return {
     page: parsedPage,
     perPage: parsedPerPage,
