e63f381202e0e28f2821c9645aeaf8e8e5661da2
Miami <alex.miami.petrov@gmail.com>
2024-11-28 16:19:37 +0200

    sort_final

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -2,7 +2,7 @@ import { SORT_ORDER } from '../constans/index.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnowOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
-  if (isKnownOrder) return sortOrder;
+  if (isKnowOrder) return sortOrder;
   return SORT_ORDER.ASC;
 };
 
